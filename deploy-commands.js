d5df715c122496cfa176a3d0a9d18d7f3148232f
crazyfaz <crazyfazzziihh@gmail.com>
2025-06-04 20:14:20 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,16 +1,12 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
-const commands = [];
-const commandsPath = path.join(__dirname, 'src', 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(`./src/commands/${file}`);
-  commands.push(command.data.toJSON());
-}
+const commands = [
+  new SlashCommandBuilder()
+    .setName('shenji')
+    .setDescription('Sends CRAZY Shenji guide.')
+    .toJSON()
+];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
@@ -20,7 +16,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
+      { body: commands },
     );
 
     console.log('✅ Successfully reloaded application (/) commands.');
