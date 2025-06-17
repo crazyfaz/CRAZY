7b439bf4259c318d827106053f2480bb4b6d6ec4
ㄈＲΛＺƳ   亗 <crazyfazzziihh@gmail.com>
2025-06-17 14:29:26 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,26 +1,35 @@
+const { REST, Routes } = require('discord.js');
+const { readdirSync } = require('fs');
+const path = require('path');
 require('dotenv').config();
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName('shenji')
-    .setDescription('Sends CRAZY Shenji guide.')
-    .toJSON()
-];
+const commands = [];
+const commandsPath = path.join(__dirname, 'src/commands');
+const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    commands.push(command.data.toJSON());
+  } else {
+    console.warn(`⚠️ The command at ${filePath} is missing "data" or "execute".`);
+  }
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
-    console.log('🔄 Refreshing application (/) commands...');
+    console.log('⏳ Refreshing application (/) commands...');
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands },
     );
 
     console.log('✅ Successfully reloaded application (/) commands.');
   } catch (error) {
-    console.error(error);
+    console.error('❌ Failed to deploy commands:', error);
   }
 })();
